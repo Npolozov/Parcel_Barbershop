2e6b1977b62201ab603b8793688d88c148fd32bc
Nikita Polozov <npolozov0@gmail.com>
2022-11-08 13:39:55 +0200

    1

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,10 +1,20 @@
 const hero = document.querySelector('.hero-content');
 const btnNext = document.querySelector('.slider_btn_next');
 
-console.log(hero);
-console.log(btnNext);
+const images = [
+  '../images/hero/hero-img-mob.jpg',
+  '../images/hero/slider№2.jpg',
+  '../images/hero/slider№3.jpg',
+];
+
+let index = 0;
+
+console.log(hero.style.backgroundImage);
 
 btnNext.addEventListener('click', e => {
   console.log(btnNext);
-  hero.classList.add('.slider_two');
+  index += 1;
+  console.log(index);
+  console.log(`${images[index]}`);
+  hero.style.backgroundImage = `linear-gradient(rgba(25, 28, 38, 0.2), rgba(25, 28, 38, 0.2)), url(${images[index]})`;
 });
