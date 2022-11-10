d4b2ec98300b19ce76c0e1dddd8add50520a7742
Nikita Polozov <npolozov0@gmail.com>
2022-11-10 11:10:09 +0200

    1

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,49 +2,51 @@ const hero = document.querySelector('.hero-content');
 const btnNext = document.querySelector('.slider_btn_next');
 const btnPrev = document.querySelector('.slider_btn_prev');
 
-// const images = [
-//   '../images/hero/hero-img-mob.jpg',
-//   '../images/hero/slider2.jpg',
-//   '../images/hero/slider3.jpg',
-// ];
-
-// let index = 0;
-
-// console.log(hero.style.backgroundImage);
-
-// btnNext.addEventListener('click', e => {
-//   console.log(btnNext);
-//   index += 1;
-//   console.log(index);
-//   console.log(`${images[index]}`);
-//   hero.style.backgroundImage = `linear-gradient(rgba(25, 28, 38, 0.2), rgba(25, 28, 38, 0.2)), url(${images[index]})`;
-// });
-
-const classNames = ['hero-content', 'slider_two', 'slider_three'];
+const classNames = [
+  'hero-content',
+  'hero-content slider_two',
+  'hero-content slider_three',
+];
 let index = 0;
 
-localPagination();
+localSlider();
 
 btnNext.addEventListener('click', () => {
-  if (index + 1 === classNames.length) index = -1;
   index++;
-  localStorage.setItem('pagination', index);
-  if (index) {
-    hero.classList[1]
-      ? hero.classList.replace(hero.classList[1], classNames[index])
-      : hero.classList.add(classNames[index]);
-  } else {
-    hero.classList.remove(hero.classList[1]);
-  }
+  if (index === classNames.length) index = 0;
+  console.log(index);
+  localStorage.setItem('slider', index);
+  hero.className = classNames[index];
 });
 
-function localPagination() {
-  const savePagination = localStorage.getItem('pagination');
-  console.log('savePagination :>> ', savePagination);
-  if (savePagination) {
-    hero.classList.add(classNames[savePagination]);
+btnPrev.addEventListener('click', () => {
+  if (index === 0) {
+    return;
+  }
+  index -= 1;
+  hero.className = classNames[index];
+});
+
+function localSlider() {
+  const saveSlider = localStorage.getItem('slider');
+  console.log('slider :>> ', saveSlider);
+  if (saveSlider) {
+    hero.className = classNames[saveSlider];
   }
 }
+
+// btnNext.addEventListener('click', () => {
+
+//   // if (index === classNames.length) index = 0;
+//   // localStorage.setItem('slider', index);
+//   // if (index) {
+//   //   hero.classList[1]
+//   //     ? hero.classList.replace(hero.classList[1], classNames[index])
+//   //     : hero.classList.add(classNames[index]);
+//   // } else {
+//   //   hero.classList.remove(hero.classList[1]);
+//   // }
+// });
 
 // btnPrev.addEventListener('click', () => {
 //   if (index + 1 === classNames.length) index = +1;
