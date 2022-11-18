9b769f581038bb772ada6c21b1b10f436b48a0fd
Nikita Polozov <npolozov0@gmail.com>
2022-11-18 12:53:11 +0200

    1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import('./js/simpleLightbox');
 import('./js/modals');
 import('./js/change-color');
 import('./js/slider');
+import('./js/scroll-to-top');
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 AOS.init();
